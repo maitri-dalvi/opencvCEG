d247866bb470c4bac150a790cff4bd84ca40d8d0
maitri-dalvi <maitridalvi2004@gmail.com>
2024-11-17 22:29:26 +0530

    video capture

--- read.py
+++ read.py
@@ -4,9 +4,14 @@ import cv2 as cv
 
 # cv.imshow('Cat', img)
 
-capture= cv.videoCapture('Videos/dog.mp4')
+capture= cv.VideoCapture('Videos/dog.mp4')
 
 while True:
     isTrue, frame = capture.read()
-
-cv.waitKey(0)
+    cv.imshow('Video', frame)
+    
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+    
+capture.release()
+cv.destroyAllWindows
