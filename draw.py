c0c7a4d7dc9a2ac043e03ebb52c480af31b8227a
maitri-dalvi <maitridalvi2004@gmail.com>
2024-11-19 21:59:43 +0530

    pixel addedgit commit

--- draw.py
+++ draw.py
@@ -4,7 +4,11 @@ import numpy as np
 blank = np.zeros((500,500,3), dtype='uint8')
 cv.imshow('Blank', blank)
 
-blank[:] = 0, 255,0
+blank[200:300, 300:400] = 0, 255,0   #200:300, 300:400 --> pixels
 cv.imshow("Green", blank)
+
+
+cv.rectangle(blank, (0,0), (250,500), (0,255,0), thickness=2)
+cv.imshow('Rectangle', blank)
 
 cv.waitKey(0)
